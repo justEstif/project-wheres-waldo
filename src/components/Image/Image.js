3c2196e2b04b2d63f77cac1c57772199fa24c5ef
justEstif <95101485+justEstif@users.noreply.github.com>
2022-06-14 11:11:51 -0400

    remove some and fixed size

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -2,21 +2,18 @@ import styled from "styled-components";
 
 const ImgDiv = styled.div`
   /*//! change both height and width when zooming in */
-  /* min-height: 200%;
-  min-width: 200%; */
-
-  min-height: 2000px;
-  min-width: 2000px;
+  min-height: 3000px;
+  min-width: 3000px;
   background-image: url(${(props) => props.imgSrc});
-  background-size: 100% 100%;
+  background-size: cover;
   background-repeat: no-repeat;
-  border: 2px red solid;
-
   display: block;
 `;
 
-const Image = ({ handleClick, imgSrc }) => {
-  return <ImgDiv onClick={(e) => handleClick(e)} imgSrc={imgSrc}></ImgDiv>;
+const Image = ({ handleMouseOver, imgSrc }) => {
+  return (
+    <ImgDiv onMouseMove={(e) => handleMouseOver(e)} imgSrc={imgSrc}></ImgDiv>
+  );
 };
 
 export default Image;
