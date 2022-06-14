26dbd26db51fcc8d8313764e5e3d982c2857b1cf
justEstif <95101485+justEstif@users.noreply.github.com>
2022-06-14 11:10:17 -0400

    change the pointer style, used attrs for dynamic

--- src/components/Pointer/Pointer.js
+++ src/components/Pointer/Pointer.js
@@ -1,21 +1,29 @@
 import styled from "styled-components";
 
-const PointerDiv = styled.div`
-  display: ${(props) => (props.position ? "block" : "none")};
-  /* border: 2px solid red; */
+const PointerDiv = styled.div.attrs(({ cursorPos }) => ({
+  style: {
+    left: cursorPos[0] + "px",
+    top: cursorPos[1] + "px",
+  },
+}))`
+  display: block;
   border-radius: 50%;
   background-color: black;
-  /* background-color: rgba(255, 255, 255, 0.5); */
   width: 20px;
   height: 20px;
-  /* border-radius: 50%; */
-  /* transform: translate3d(-50%, -50%, 0);
-  left: ${(props) => props.position[0] + "px"};
-  top: ${(props) => props.position[1] + "px"}; */
+
+  position: absolute;
+  z-index: 2;
+  transform: translate3d(-50%, -50%, 0);
 `;
 
-const Pointer = ({ position }) => {
-  return <PointerDiv position={position}></PointerDiv>;
+const Pointer = ({ cursorPos, handleClick }) => {
+  return (
+    <PointerDiv
+      onClick={(e) => handleClick(e)}
+      cursorPos={cursorPos}
+    ></PointerDiv>
+  );
 };
 
 export default Pointer;
