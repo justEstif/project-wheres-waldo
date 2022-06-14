43fe8820c576328ee49fab312b930200138bf992
justEstif <95101485+justEstif@users.noreply.github.com>
2022-06-14 11:12:58 -0400

    added form inputs, will refactor later

--- src/components/DropdownForm/DropdownForm.js
+++ src/components/DropdownForm/DropdownForm.js
@@ -1,44 +1,73 @@
 import styled from "styled-components";
-const DropdownDiv = styled.div`
-  display: ${(props) => (props.show ? "block" : "none")};
+
+const DropdownDiv = styled.div.attrs((props) => ({
+  style: {
+    display: props.display,
+    left: props.clickedPos[0] + "px",
+    top: props.clickedPos[1] + "px",
+  },
+}))`
+  position: absolute;
+  z-index: 2;
 `;
 
 const FormDiv = styled.form`
-  /* display: flex; */
-  /* flex-direction: column; */
-
-  /* background-color: black; */
-  /* height: 100px; */
-  /*  width: 100%; */
-
   display: flex;
   flex-direction: column;
   align-items: flex-start;
+  justify-content: center;
   background-color: black;
   color: white;
-  width: 180px;
+  /* width: 180px; */
+  padding: 20px 40px;
+
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
 `;
 
-const DropdownForm = ({ show }) => {
+const InputSty = styled.input`
+  opacity: 0;
+`;
+const LabelSty = styled.label`
+  &:hover {
+    color: green;
+  }
+`;
+const DropdownForm = ({ clickedPos, clicked, handleSubmit }) => {
+  const display = clicked ? "block" : "none";
   return (
-    <DropdownDiv show={show}>
+    <DropdownDiv display={display} clickedPos={clickedPos}>
+      {/* //! change the form values*/}
       <FormDiv>
         <p>
-          <input type="radio" id="html" name="fav_language" value="HTML" />
-          <label htmlFor="html">HTML</label>
-        </p>
-        <p>
-          <input type="radio" id="css" name="fav_language" value="CSS" />
-          <label htmlFor="css">CSS</label>
-        </p>
-        <p>
-          <input
+          <InputSty
+            onChange={() => handleSubmit}
             type="radio"
-            id="javascript"
+            id="waldo"
             name="fav_language"
-            value="JavaScript"
+            value=""
           />
-          <label htmlFor="javascript">JavaScript</label>
+          <LabelSty onClick={() => handleSubmit()} htmlFor="waldo">
+            Waldo
+          </LabelSty>
+        </p>
+        <p>
+          <InputSty type="radio" id="greece" name="fav_language" value="" />
+          <LabelSty onClick={() => handleSubmit()} htmlFor="greece">
+            Greece
+          </LabelSty>
+        </p>
+        <p>
+          <InputSty type="radio" id="algeria" name="fav_language" value="" />
+          <LabelSty
+            onClick={() => handleSubmit()}
+            type="radio"
+            htmlFor="algeria"
+          >
+            Algeria
+          </LabelSty>
         </p>
       </FormDiv>
     </DropdownDiv>
