98c37340ec20a29ce2ba1e8b34855a33f84f813c
justEstif <95101485+justEstif@users.noreply.github.com>
2022-06-14 11:16:59 -0400

    new imports, styling, functions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { Fragment, useState } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 import { rPlace } from "./assets";
-import { Overlay, Image } from "./components/index";
+import { Header, Overlay, Image } from "./components/index";
 
 const GlobalStyle = createGlobalStyle`
-html {
-  box-sizing: border-box;
-}
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-body {
-  min-height: 100vh;
-  padding: 0;
-  margin: 0;
-}
+  html {
+    box-sizing: border-box;
+  }
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+  body {
+    min-height: 100vh;
+    padding: 0;
+    margin: 0;
+  }
+  ::-webkit-scrollbar { 
+    display: none;
+  }
 `;
 
 const AppDiv = styled.div`
@@ -26,26 +31,47 @@ const AppDiv = styled.div`
 
   display: flex;
   flex-direction: column;
-  /* overflow: hidden; */
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 `;
 
-const App = ({}) => {
-  const [position, setPosition] = useState([]);
-  const handleClick = (e) => {
+const App = () => {
+  const [clicked, setClicked] = useState(false);
+  const [cursorPos, setCursorPos] = useState([]);
+  const [clickedPos, setClickedPos] = useState([]);
+
+  const getCoordinates = (e) => {
     const bnds = e.target.getBoundingClientRect();
     const x = e.clientX - bnds.left;
     const y = e.clientY - bnds.top;
-    // ! remove this later
-    console.log(`(${x}, ${y})`);
-    setPosition([x, y]);
+    return [x, y];
+  };
+
+  const handleClick = () => {
+    setClickedPos(cursorPos);
+    setClicked(!clicked);
+  };
+
+  const handleMouseOver = (e) => setCursorPos(getCoordinates(e));
+
+  const handleSubmit = (e) => {
+    console.log("submit");
+    setClicked(false);
   };
 
   return (
     <Fragment>
       <GlobalStyle />
       <AppDiv>
-        <Overlay position={position}></Overlay>
-        <Image handleClick={handleClick} imgSrc={rPlace}></Image>
+        <Header></Header>
+        <Overlay
+          clicked={clicked}
+          cursorPos={cursorPos}
+          handleClick={handleClick}
+          clickedPos={clickedPos}
+          handleSubmit={handleSubmit}
+        ></Overlay>
+        <Image handleMouseOver={handleMouseOver} imgSrc={rPlace}></Image>
       </AppDiv>
     </Fragment>
   );
