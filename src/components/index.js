d2a8278e33630dbe50d5a292757f577cfa1d40f0
justEstif <95101485+justEstif@users.noreply.github.com>
2022-06-14 11:16:39 -0400

    added header component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Overlay from "../components/Overlay/Overlay";
 import Image from "../components/Image/Image";
+import Header from "../components/Header/Header";
 import Pointer from "../components/Pointer/Pointer";
 import DropdownForm from "./DropdownForm/DropdownForm";
 
-export { Pointer, Overlay, Image, DropdownForm };
+export { Pointer, Overlay, Image, DropdownForm, Header };
